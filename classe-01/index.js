b4d2088a00dbaaf3918282eea41fdd01ce1d1f1e
Rodrigo Pereira Duarte da Silva <rodrigoduartesilva@yahoo.com.br>
2022-11-03 19:50:17 -0300

    Classe 01 - Finalizado

--- classe-01/index.js
+++ classe-01/index.js
@@ -26,6 +26,18 @@ const prova = {
     ]
 };
 
-function corrigirProva(prova) {
+let soma = 0;
 
+function corrigirProva(prova) {
+    for (let i in prova.questoes) {
+        if (prova.questoes[i].resposta === prova.questoes[i].correta) {
+            soma++;
+        }
+    }
+
+    return soma;
 }
+
+const resultado = corrigirProva(prova);
+
+console.log(`O aluno(a) ${prova.aluno} acertou ${resultado} questões e obteve nota ${resultado * 2}.`);
