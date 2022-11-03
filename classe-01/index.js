33327e152e58dd287d65b37d6e58ec43475b7d68
Rodrigo Pereira Duarte da Silva <rodrigoduartesilva@yahoo.com.br>
2022-11-03 18:25:07 -0300

    Primeiro commit

---
+++ classe-01/index.js
@@ -0,0 +1,31 @@
+const prova = {
+    aluno: "João",
+    materia: "Português",
+    valor: 10,
+    questoes: [
+        {
+            resposta: "a",
+            correta: "b"
+        },
+        {
+            resposta: "c",
+            correta: "c"
+        },
+        {
+            resposta: "e",
+            correta: "b"
+        },
+        {
+            resposta: "b",
+            correta: "b"
+        },
+        {
+            resposta: "c",
+            correta: "c"
+        }
+    ]
+};
+
+function corrigirProva(prova) {
+
+}
