2697c2ea8a2e3a2429835a45d8cac294ca506603
Rodrigo Pereira Duarte da Silva <rodrigoduartesilva@yahoo.com.br>
2022-11-04 19:27:51 -0300

    Primeiro commit

---
+++ extra-03/index.js
@@ -0,0 +1,24 @@
+const carrinho = {
+    nomeDoCliente: "Guido Bernal",
+    produtos: [
+        {
+            id: 1,
+            nome: "Camisa",
+            qtd: 3,
+            precoUnit: 3000
+        },
+        {
+            id: 2,
+            nome: "Bermuda",
+            qtd: 2,
+            precoUnit: 5000
+        }
+    ]
+}
+
+
+function imprimirResumoDoCarrinho(carrinho) {
+    console.log(carrinho);
+}
+
+imprimirResumoDoCarrinho(carrinho);
