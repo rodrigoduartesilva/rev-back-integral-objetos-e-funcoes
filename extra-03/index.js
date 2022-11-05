39e9a98b41417357b4d8e2f2c32f81e2f7b6fb80
Rodrigo Pereira Duarte da Silva <rodrigoduartesilva@yahoo.com.br>
2022-11-04 22:30:15 -0300

    Método imprimirResumo() criado.

--- extra-03/index.js
+++ extra-03/index.js
@@ -13,20 +13,20 @@ const carrinho = {
             qtd: 2,
             precoUnit: 5000
         }
-    ]
-}
+    ],
 
-let itensCarrinho = 0;
-let totalPagar = 0;
+    imprimirResumo: function () {
+        let itensCarrinho = 0;
+        let totalPagar = 0;
 
-function imprimirResumoDoCarrinho(carrinho) {
-    for (let item in carrinho.produtos) {
-        itensCarrinho += carrinho.produtos[item].qtd;
-        totalPagar += (carrinho.produtos[item].qtd * carrinho.produtos[item].precoUnit);
-    }
-    console.log(`Cliente: ${carrinho.nomeDoCliente} 
+        for (let item in this.produtos) {
+            itensCarrinho += this.produtos[item].qtd;
+            totalPagar += (this.produtos[item].qtd * this.produtos[item].precoUnit);
+        }
+        console.log(`Cliente: ${this.nomeDoCliente} 
          Total de itens: ${itensCarrinho}
          Total a pagar: R$ ${((totalPagar) / 100).toFixed(2)}`);
+    }
 }
 
-imprimirResumoDoCarrinho(carrinho);
+carrinho.imprimirResumo();
